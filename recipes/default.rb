f2eae469be5ff4b7c991c29ebb081e091bb276a6
Konstantin Gredeskoul & Richard Millan <wanelo-pair@wanelo.com>
2013-06-21 17:53:34 -0700

    WIP changes to get this working...

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,7 +26,7 @@ package "openswan" do
 end
 
 execute "turn on SNAT" do
-  command "iptables -t nat -I POSTROUTING -o eth0 -j SNAT --to $(PUBIP)"
+  command "iptables -t nat -I POSTROUTING -o eth0 -j SNAT --to #{node['ipaddress']}"
 end
 
 execute "turn on ipv4 forwarding" do
@@ -34,47 +34,46 @@ execute "turn on ipv4 forwarding" do
   not_if "grep 1 /proc/sys/net/ipv4/ip_forward"
 end
 
-script "turn off redirects" do
-  user "root"
-  cwd "/tmp"
+bash "turn off redirects" do
   code <<-EOH
-  echo 0 > /proc/sys/net/ipv4/conf/all/send_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/bond0/send_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/default/send_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/dummy0/send_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/eql/send_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/eth0/send_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/eth1/send_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/ip6tnl0/send_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/lo/send_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/sit0/send_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/tunl0/send_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/bond0/accept_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/default/accept_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/dummy0/accept_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/eql/accept_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/eth0/accept_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/eth1/accept_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/ip6tnl0/accept_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/lo/accept_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/sit0/accept_redirects
-  echo 0 > /proc/sys/net/ipv4/conf/tunl0/accept_redirects
+echo 0 > /proc/sys/net/ipv4/conf/all/send_redirects
+echo 0 > /proc/sys/net/ipv4/conf/bond0/send_redirects
+echo 0 > /proc/sys/net/ipv4/conf/default/send_redirects
+echo 0 > /proc/sys/net/ipv4/conf/dummy0/send_redirects
+echo 0 > /proc/sys/net/ipv4/conf/eql/send_redirects
+echo 0 > /proc/sys/net/ipv4/conf/eth0/send_redirects
+echo 0 > /proc/sys/net/ipv4/conf/eth1/send_redirects
+echo 0 > /proc/sys/net/ipv4/conf/ip6tnl0/send_redirects
+echo 0 > /proc/sys/net/ipv4/conf/lo/send_redirects
+echo 0 > /proc/sys/net/ipv4/conf/sit0/send_redirects
+echo 0 > /proc/sys/net/ipv4/conf/tunl0/send_redirects
+echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects
+echo 0 > /proc/sys/net/ipv4/conf/bond0/accept_redirects
+echo 0 > /proc/sys/net/ipv4/conf/default/accept_redirects
+echo 0 > /proc/sys/net/ipv4/conf/dummy0/accept_redirects
+echo 0 > /proc/sys/net/ipv4/conf/eql/accept_redirects
+echo 0 > /proc/sys/net/ipv4/conf/eth0/accept_redirects
+echo 0 > /proc/sys/net/ipv4/conf/eth1/accept_redirects
+echo 0 > /proc/sys/net/ipv4/conf/ip6tnl0/accept_redirects
+echo 0 > /proc/sys/net/ipv4/conf/lo/accept_redirects
+echo 0 > /proc/sys/net/ipv4/conf/sit0/accept_redirects
+echo 0 > /proc/sys/net/ipv4/conf/tunl0/accept_redirects
   EOH
   not_if "grep 0 /proc/sys/net/ipv4/conf/tunl0/accept_redirects"
 end
 
 ["ppp", "xl2tpd"].each do |p|
   package p
-  action :install
 end
 
 template "#{node['openswan']['xl2tpd_path']}/xl2tpd.conf" do
-  source "xl3tpd.conf.erb"
+  source "xl2tpd.conf.erb"
+  notifies :reload, "service[xl2tpd]"
 end 
 
-template "#{node['openswan']['xl2tpd_path']}/options.xl2tpd" do
+template "#{node['openswan']['ppp_path']}/options.xl2tpd" do
   source "options.xl2tpd.erb"
+  notifies :reload, "service[xl2tpd]"
 end
 
 template "#{node['openswan']['ppp_path']}/chap-secrets" do
@@ -82,21 +81,21 @@ template "#{node['openswan']['ppp_path']}/chap-secrets" do
   notifies :reload, "service[xl2tpd]"
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/linux-image-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb" do
+service "xl2tpd" do
+  supports :status => true, :restart => true, :start => true, :stop => true
+  action [ :enable, :start ]
+end
+
+remote_file "/var/tmp/linux-image-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb" do
   source "http://l03.ryan.net/data/linux-image-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb"
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/linux-headers-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb" do
+remote_file "/var/tmp/linux-headers-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb" do
   source "http://l03.ryan.net/data/linux-headers-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb"
 end
 
-["linux-headers-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64", "linux-image-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64"].each do |p|
-  dpkg_package p
-  action :install
-end
-
-execute "restart xl2tpd and ipsec" do
-  command "/etc/init.d/xl2tpd restart && /etc/init.d/ipsec restart"
+execute "install custom joyent linux headers" do
+  command "dpkg --install /var/tmp/linux-headers-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb && dpkg --install /var/tmp/linux-image-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb"
 end
 
 execute "restart xl2tpd and ipsec" do
