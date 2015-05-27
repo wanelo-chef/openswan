b0e78d846361237e0bfcca2197c1c4ba568dc777
tboeckmann <trb@bespecific.co.uk>
2015-05-27 12:27:38 +0100

    Update _install_headers.rb
    File not found

--- recipes/_install_headers.rb
+++ recipes/_install_headers.rb
@@ -1,13 +1,13 @@
-remote_file "/var/tmp/linux-image-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb" do
-  source "http://l03.ryan.net/data/linux-image-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb"
-end
+#remote_file "/var/tmp/linux-image-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb" do
+#  source "http://l03.ryan.net/data/linux-image-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb"
+#end
 
-remote_file "/var/tmp/linux-headers-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb" do
-  source "http://l03.ryan.net/data/linux-headers-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb"
-end
+#remote_file "/var/tmp/linux-headers-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb" do
+#  source "http://l03.ryan.net/data/linux-headers-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb"
+#end
 
-execute "install custom joyent linux headers" do
-  command "dpkg --install --force-confnew /var/tmp/linux-headers-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb && dpkg --install --force-confnew /var/tmp/linux-image-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb"
-  not_if "ls /lib/modules/3.8.4-joyent-ubuntu-12-opt/kernel"
-end
+#execute "install custom joyent linux headers" do
+#  command "dpkg --install --force-confnew /var/tmp/linux-headers-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb && dpkg --install --force-confnew /var/tmp/linux-image-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb"
+#  not_if "ls /lib/modules/3.8.4-joyent-ubuntu-12-opt/kernel"
+#end
 
