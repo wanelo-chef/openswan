92d2dda4ed0ee208a5c643db00e7c9707509cfd2
Eric Saxby <sax@livinginthepast.org>
2015-06-11 17:50:30 -0700

    Use manta url for .deb header files

--- recipes/_install_headers.rb
+++ recipes/_install_headers.rb
@@ -1,9 +1,9 @@
 remote_file "/var/tmp/linux-image-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb" do
-  source "http://l03.ryan.net/data/linux-image-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb"
+  source "https://us-east.manta.joyent.com/wanelo/public/cache/openswan/linux-image-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb"
 end
 
 remote_file "/var/tmp/linux-headers-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb" do
-  source "http://l03.ryan.net/data/linux-headers-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb"
+  source "https://us-east.manta.joyent.com/wanelo/public/cache/openswan/linux-headers-3.8.4-joyent-ubuntu-12-opt_1.0.0_amd64.deb"
 end
 
 execute "install custom joyent linux headers" do
